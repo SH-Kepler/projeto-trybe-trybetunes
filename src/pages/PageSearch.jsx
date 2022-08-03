584044670405e7b6f3490dd06d304d68b753ba7b
Sheifer Kepler <sheifer.kda@gmail.com>
2022-08-03 20:13:13 -0300

    requisito 5

--- src/pages/PageSearch.jsx
+++ src/pages/PageSearch.jsx
@@ -1,12 +1,53 @@
 import React from 'react';
 import Header from '../components/Header';
 
+const inittialState = {
+  inputValue: '',
+  isDisabled: true,
+};
 class PageSearch extends React.Component {
+  constructor() {
+    super();
+
+    this.state = inittialState;
+  }
+
+  enablaButton = () => {
+    const { inputValue } = this.state;
+    const maxLangth = 2;
+    const inputSize = inputValue.length < maxLangth;
+    return inputSize;
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState(() => ({
+      [name]: value,
+    }), () => this.setState({ isDisabled: this.enablaButton() }));
+  }
+
   render() {
+    const { inputValue, isDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <h1>page-search</h1>
+        <form action="">
+          <input
+            value={ inputValue }
+            name="inputValue"
+            onChange={ this.handleChange }
+            data-testid="search-artist-input"
+            placeholder="Nome do Artista"
+            type="text"
+          />
+          <button
+            disabled={ isDisabled }
+            data-testid="search-artist-button"
+            type="submit"
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
