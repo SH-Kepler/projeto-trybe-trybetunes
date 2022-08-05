3f709b2eb76adeaf8c4c29506795e5105beae311
Sheifer Kepler <sheifer.kda@gmail.com>
2022-08-05 11:36:30 -0300

    requisito 6

--- src/pages/PageSearch.jsx
+++ src/pages/PageSearch.jsx
@@ -1,15 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 const inittialState = {
+  inputValueHunter: '',
   inputValue: '',
   isDisabled: true,
+  isLoading: false,
+  fail: false,
+  music: [],
+  realArtist: true,
 };
 class PageSearch extends React.Component {
   constructor() {
     super();
 
     this.state = inittialState;
+  }
+
+  async componentDidMount() {
+    const { inputValue } = this.state;
+    this.setState({ inputValueHunter: inputValue });
+    this.setState({ inputValue: '' });
+    const data = await searchAlbumsAPI(inputValue);
+    this.setState({ isLoading: false });
+    if (data.length > 0) {
+      this.setState({ music: data });
+    }
   }
 
   enablaButton = () => {
@@ -26,28 +44,68 @@ class PageSearch extends React.Component {
     }), () => this.setState({ isDisabled: this.enablaButton() }));
   }
 
+  searchArtist = (event) => {
+    const { music } = this.state;
+    if (music.length === 0) {
+      this.setState({
+        realArtist: false,
+      });
+    }
+    this.setState({
+      isLoading: true,
+    });
+    event.preventDefault();
+    this.componentDidMount();
+  }
+
   render() {
-    const { inputValue, isDisabled } = this.state;
+    const { inputValue, isDisabled, isLoading,
+      music, realArtist, inputValueHunter } = this.state;
     return (
       <div data-testid="page-search">
-        <Header />
-        <form action="">
-          <input
-            value={ inputValue }
-            name="inputValue"
-            onChange={ this.handleChange }
-            data-testid="search-artist-input"
-            placeholder="Nome do Artista"
-            type="text"
-          />
-          <button
-            disabled={ isDisabled }
-            data-testid="search-artist-button"
-            type="submit"
-          >
-            Pesquisar
-          </button>
-        </form>
+        { isLoading ? <div>Carregando...</div>
+          : (
+            <div>
+              <Header />
+              <form onSubmit={ this.searchArtist }>
+                <input
+                  value={ inputValue }
+                  name="inputValue"
+                  onChange={ this.handleChange }
+                  data-testid="search-artist-input"
+                  placeholder="Nome do Artista"
+                  type="text"
+                />
+                <button
+                  disabled={ isDisabled }
+                  data-testid="search-artist-button"
+                  type="submit"
+                >
+                  Pesquisar
+                </button>
+              </form>
+              {realArtist === false && music.length === 0
+              && <p>Nenhum álbum foi encontrado</p>}
+              {music.length > 0 ? <p>{`Resultado de álbuns de: ${inputValueHunter}`}</p>
+                : null }
+              {music.map((element) => (
+                <Link
+                  to={ `/album/${element.collectionId}` }
+                  className="collections"
+                  data-testid={ `link-to-album-${element.collectionId}` }
+                  key={ `${element.artistId}
+                  ${element.collectionId}` }
+                >
+                  <img src={ element.artworkUrl100 } alt="artworkUrl100" />
+                  <p>{element.collectionName}</p>
+                  <p>{element.collectionPrice}</p>
+                  <p>{element.releaseDate}</p>
+                  <p>{element.trackCount}</p>
+                </Link>
+              ))}
+            </div>
+          )}
+
       </div>
     );
   }
